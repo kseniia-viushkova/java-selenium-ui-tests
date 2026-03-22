4da32614939fc57b5a9c95f58f1e93609d26698a
Ksenia Vyushkova <ksenia.vyushkova@gmail.com>
2026-03-22 20:44:18 +0400

    Add logging

--- src/test/java/testBase/BaseTest.java
+++ src/test/java/testBase/BaseTest.java
@@ -1,5 +1,8 @@
 package testBase;
 
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -11,8 +14,12 @@ public class BaseTest {
 
     public WebDriver driver;
 
+    public Logger logger;
+
     @BeforeClass
     public void setup() {
+        logger = LogManager.getLogger(this.getClass());
+
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
